6ad495fc36f9a2306bcff7f186056841de27df97
lucalabarbera <lucalabarbera@icloud.com>
2020-01-30 12:51:28 +0000

    06.1

---
+++ RestFull-CRUD/routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql')
+const config = {
+  user: 'labarbera.luca',  //Vostro user name
+  password: 'xxx123#', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: 'labarbera.luca', //(Nome del DB)
+}
+
+/* GET users listing. */
+router.get('/attori', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select * from Customer', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+  });
+});
+
+module.exports = router;
